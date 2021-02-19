b82a0c0af417f34cbc6badde10b6ebf9fad8d493
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-02-19 21:20:23 +0200

    add monthYearHeadingComponent

---
+++ src/calendar/current-calendar-body/mounth-year/index.jsx
@@ -0,0 +1,24 @@
+import{Component} from 'react';
+import{format} from 'date-fns';
+import style from '../mounth-year/mouthYear.module.scss';
+
+class MounthYear extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+  
+
+  render(){
+    const {day}=this.props
+
+    return(
+        <div className = {style.container}>
+          <button onClick = {this.props.handlePrev}>Prev</button>
+          <h1>{format(day, 'MMMM yyyy')}</h1>
+          <button onClick ={this.props.handleNext}>Next</button>
+        </div>  
+    )
+  }
+}
+export default MounthYear;
